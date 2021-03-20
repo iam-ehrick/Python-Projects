106dda348156ffd6acd91e4b87f2a8758a31f539
Ehrick <76269835+iam-ehrick@users.noreply.github.com>
2021-03-20 15:57:02 +0100

    Add files via upload
    A basic login system

---
+++ PasswordSystem.py
@@ -0,0 +1,20 @@
+
+password = ''
+storedPassword = 'kudi'
+allowedTries = 3
+attempts = 0
+
+
+
+while password != storedPassword:
+    password = input('Enter password: ')
+    attempts += 1
+    if attempts == allowedTries and password != storedPassword:
+        print('Too many tries. Reload page.')
+        break
+    elif attempts <= allowedTries and password == storedPassword:
+        continue
+    else:
+        print('You have used', attempts, 'attempt(s).')
+else:
+    print('Access granted.')
